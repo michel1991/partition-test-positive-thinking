126791c091b8d764e065f5be38d00c07b1ff2295
michel1991 <michelmvoumamvouma@gmail.com>
2022-02-02 11:10:59 +0100

    code: class that allow to make partition and test

---
+++ src/main/java/org/example/Partition.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Partition {
+
+    public List takeLastGroup(List<Integer> list, int numberOfElementToTake){
+        List lastGroupList = new ArrayList();
+        if(numberOfElementToTake > 0){
+            int size = list.size();
+            int copy = numberOfElementToTake;
+            for (int i = 1; i <= numberOfElementToTake ; i++) {
+                lastGroupList.add(list.get(size - copy));
+                copy--;
+            }
+        }
+
+        return lastGroupList;
+    }
+
+    public List<List> takeAllFirstGroup(List<Integer> list, int restToLoop, int numberOfElementToTake){
+        List<List>  allFirstGroups = new ArrayList<>();
+        for (int i = 0; i < restToLoop; i+=numberOfElementToTake) {
+            List<Integer>  subGroup = new ArrayList<>();
+            int copyNumberOfJump = i;
+            while(subGroup.size() < numberOfElementToTake){
+                subGroup.add(list.get(copyNumberOfJump));
+                copyNumberOfJump++;
+            }
+            allFirstGroups.add(subGroup);
+
+        }
+        return  allFirstGroups;
+    }
+
+
+
+    public List<List> makePartition(List<Integer> list, int numberOfElementPerSubList)  throws IllegalArgumentException
+    {
+        if(numberOfElementPerSubList <= 0){
+            throw new IllegalArgumentException("number of element per list must be positive number");
+        }
+        int copyFirstSize = list.size();
+        List takeLastGroup = this.takeLastGroup(list, copyFirstSize % numberOfElementPerSubList);
+        int restOfElementToCopy = copyFirstSize - (copyFirstSize%numberOfElementPerSubList);
+        List<List> listOfPartition = this.takeAllFirstGroup(list, restOfElementToCopy, numberOfElementPerSubList);
+        if(takeLastGroup.size() >0 ){
+            listOfPartition.add(takeLastGroup);
+        }
+        return listOfPartition;
+
+    }
+}
---
+++ src/test/java/org/example/PartitionTest.java
@@ -0,0 +1,113 @@
+package org.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PartitionTest{
+    private List<Integer> listOfValues;
+    @Before
+    public void setUp() {
+        this.listOfValues = new ArrayList<>();
+        listOfValues.add(1);
+        listOfValues.add(2);
+        listOfValues.add(3);
+        listOfValues.add(4);
+        listOfValues.add(5);
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void makePartitionWithTwoNumberPerSubList() {
+        Partition partition = new Partition();
+        List<List> actual = partition.makePartition(this.listOfValues, 2);
+        List<List> expected = new ArrayList<>();
+        List firstSubGroup = new ArrayList<Integer>();
+        List secondSubGroup = new ArrayList<Integer>();
+        List thirdSubGroup = new ArrayList<Integer>();
+
+        firstSubGroup.add(1);
+        firstSubGroup.add(2);
+
+
+        secondSubGroup.add(3);
+        secondSubGroup.add(4);
+
+        thirdSubGroup.add(5);
+
+        expected.add(firstSubGroup);
+        expected.add(secondSubGroup);
+        expected.add(thirdSubGroup);
+        assertArrayEquals(expected.toArray(), actual.toArray());
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void makePartitionWithThreeNumberPerSubList() {
+        Partition partition = new Partition();
+        List<List> actual = partition.makePartition(this.listOfValues, 3);
+        List<List> expected = new ArrayList<>();
+        List firstSubGroup = new ArrayList<Integer>();
+        List secondSubGroup = new ArrayList<Integer>();
+
+        firstSubGroup.add(1);
+        firstSubGroup.add(2);
+        firstSubGroup.add(3);
+
+
+        secondSubGroup.add(4);
+        secondSubGroup.add(5);
+
+
+        expected.add(firstSubGroup);
+        expected.add(secondSubGroup);
+        assertArrayEquals(expected.toArray(), actual.toArray());
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void makePartitionWithOneNumberPerSubList() {
+        Partition partition = new Partition();
+        List<List> actual = partition.makePartition(this.listOfValues, 1);
+        List<List> expected = new ArrayList<>();
+        List firstSubGroup = new ArrayList<Integer>();
+        List secondSubGroup = new ArrayList<Integer>();
+        List threeSubGroup = new ArrayList<Integer>();
+        List fourSubGroup = new ArrayList<Integer>();
+        List fiveSubGroup = new ArrayList<Integer>();
+
+        firstSubGroup.add(1);
+        secondSubGroup.add(2);
+        threeSubGroup.add(3);
+        fourSubGroup.add(4);
+        fiveSubGroup.add(5);
+
+        expected.add(firstSubGroup);
+        expected.add(secondSubGroup);
+        expected.add(threeSubGroup);
+        expected.add(fourSubGroup);
+        expected.add(fiveSubGroup);
+
+        assertArrayEquals(expected.toArray(), actual.toArray());
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void makePartitionWithNegativeNumberPerSubList() {
+        Partition partition = new Partition();
+        partition.makePartition(this.listOfValues, -1);
+    }
+
+}
